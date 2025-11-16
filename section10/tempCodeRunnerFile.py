3632e0fcf1a827f19dc5ae5cdf97bd82bdc6a11b
DevSharma2458 <devsharma24003@gmail.com>
2025-11-16 21:41:43 +0530

    regex  commit

---
+++ section10/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+# Search for a pattern
+# match = re.search("brown", text)
+# print(match)
+# if match:
+#     print("Match found!")
+#     print("Start index:", match.start())
+#     print("End index:", match.end())
